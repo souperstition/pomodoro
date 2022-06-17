e48fa8c3e7af76bba418930bc36e9fcb18f8a5fd
Danielle Lyle <karendlyle@gmail.com>
2022-06-17 11:46:09 +0200

    comment

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import * as ReactDOM from 'react-dom';
 import * as React from 'react';
 import App from './App';
 
+// fcc tests don't pass with new render method
 ReactDOM.render(<App />, document.getElementById('root'));
