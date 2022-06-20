1394ba9be03aac42ad29271e35262a763477dc01
Danielle Lyle <karendlyle@gmail.com>
2022-06-20 10:40:08 +0200

    swap buttons

--- src/components/Break.jsx
+++ src/components/Break.jsx
@@ -4,8 +4,8 @@ import { ReactComponent as MinusIcon } from './icons/minusIcon.svg';
 const Break = ({ breakLength, handleBreakDecrease, handleBreakIncrease, isRunning }) => {
 	return (
 		<div className="w-1/2 flex justify-center items-center gap-x-2" role="group">
-			<button disabled={isRunning} onClick={handleBreakIncrease} id="break-increment">
-				<PlusIcon className="cursor-pointer transition-colors duration-300 text-amber-500 hover:text-amber-700" />
+			<button disabled={isRunning} onClick={handleBreakDecrease} id="break-decrement">
+				<MinusIcon className="cursor-pointer transition-colors duration-300 text-amber-500 hover:text-amber-700" />
 			</button>
 			<div className="flex flex-col justify-center">
 				<h3 className="text-center font-bold" id="break-label">
@@ -15,8 +15,8 @@ const Break = ({ breakLength, handleBreakDecrease, handleBreakIncrease, isRunnin
 					<span id="break-length">{breakLength}</span> minutes
 				</p>
 			</div>
-			<button disabled={isRunning} onClick={handleBreakDecrease} id="break-decrement">
-				<MinusIcon className="cursor-pointer transition-colors duration-300 text-amber-500 hover:text-amber-700" />
+			<button disabled={isRunning} onClick={handleBreakIncrease} id="break-increment">
+				<PlusIcon className="cursor-pointer transition-colors duration-300 text-amber-500 hover:text-amber-700" />
 			</button>
 		</div>
 	);
--- src/components/Session.jsx
+++ src/components/Session.jsx
@@ -4,8 +4,8 @@ import { ReactComponent as MinusIcon } from './icons/minusIcon.svg';
 const Session = ({ isRunning, sessionLength, handleSessionDecrease, handleSessionIncrease }) => {
 	return (
 		<div className="w-1/2 flex justify-center items-center gap-x-2" role="group">
-			<button disabled={isRunning} onClick={handleSessionIncrease} id="session-increment">
-				<PlusIcon className="cursor-pointer transition-colors duration-300 text-amber-500 hover:text-amber-700" />
+			<button disabled={isRunning} onClick={handleSessionDecrease} id="session-decrement">
+				<MinusIcon className="cursor-pointer transition-colors duration-300 text-amber-500 hover:text-amber-700" />
 			</button>
 			<div className="flex flex-col justify-center">
 				<h3 className="text-center font-bold" id="session-label">
@@ -15,8 +15,8 @@ const Session = ({ isRunning, sessionLength, handleSessionDecrease, handleSessio
 					<span id="session-length">{sessionLength}</span> minutes
 				</p>
 			</div>
-			<button disabled={isRunning} onClick={handleSessionDecrease} id="session-decrement">
-				<MinusIcon className="cursor-pointer transition-colors duration-300 text-amber-500 hover:text-amber-700" />
+			<button disabled={isRunning} onClick={handleSessionIncrease} id="session-increment">
+				<PlusIcon className="cursor-pointer transition-colors duration-300 text-amber-500 hover:text-amber-700" />
 			</button>
 		</div>
 	);
